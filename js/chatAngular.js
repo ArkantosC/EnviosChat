1281e5e00fe670b644bd28a0e52073ecb54b3d9f
DiegoC <diegocortes@MacBook-Pro-de-Diego.local>
2015-10-12 23:24:47 -0500

    se agrega salir login

--- js/chatAngular.js
+++ js/chatAngular.js
@@ -1,11 +1,25 @@
 angular.module('chatusb', [])
-	.controller('chatAngularController', function($scope, $interval) {
+	.controller('chatAngularController', function($scope, $interval, $window) {
 		
+		var nombreUsuario = '';
+
+		function autoDataCallback(authData) {
+			if(authData) {
+				nombreUsuario = authData.password.email
+				console.log("Usuario " +  authData.password.email + " esta logeado.");
+			}else{
+				$window.location.href = 'login.html'
+				console.log("El usuario no esta logeado");
+			}
+		}
+
 		//var objFirebase = new Firebase('https://chatusb.firebaseio.com/');
 		//var objFirebase = new Firebase('https://ce4r7beysrf.firebaseio-demo.com/');
 		var objFirebase = new Firebase('https://chatusbtest.firebaseio.com/');
+		objFirebase.onAuth(autoDataCallback);		
+		
 		$scope.mensajes = [];
-		$scope.nombre = 'Diego';
+		$scope.nombre = nombreUsuario;
 
 		objFirebase.on('child_added', function(snapshot) {
 		 		var message = snapshot.val();
@@ -27,12 +41,14 @@ angular.module('chatusb', [])
 			$scope.mensaje = '';
 		}
 
-		$scope.auth_twitter = function() {
-			$scope.nombre = 'autenticado por Twitter' + $scope.nombre; 	
+		
+		$scope.salir = function() {
+			objFirebase.unauth();
 		}
 
+/*
 		$interval( function(){
         	$scope.$apply();
-    	}, 5000 );
-
+    	}, 7000 );
+*/
 	});
